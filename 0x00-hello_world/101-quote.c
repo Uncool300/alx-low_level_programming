b6ae565b1576e2a15d670260cbbfef0496318fee
Uncool300 <uncoolyemi@gmail.com>
2023-08-14 18:16:03 -0700

    printing write 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - printing without printf and puts expert mode
  *
- * Return: 1 (Success)
+ * Return: always (1)
+ *
  */
 int main(void)
 {
-	Write("and that piece of art is useful\" - Dora Korpar, 2015-10-19);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
